31774fa41eb666c81d125726136fb30e3c518592
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-12-20 20:51:30 +0100

    challenge 01

---
+++ challenge-01/solution.js
@@ -0,0 +1,22 @@
+export default function contarOvejas(ovejas) {
+    // aquí tu magia
+    const result = ovejas.filter(oveja => oveja.color == 'rojo' && oveja.name.toLowerCase().includes('n') & oveja.name.toLowerCase().includes('a'));
+
+    return result
+}
+
+const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo' },
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo' }
+]
+
+const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
+
+// [{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
